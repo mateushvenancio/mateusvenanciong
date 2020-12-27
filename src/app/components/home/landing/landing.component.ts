6ff1dce8e48dc8cfafe106a1723f763e297ae987
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2020-12-27 11:03:24 -0300

    Menu hamburguer funcionando

--- src/app/components/home/landing/landing.component.ts
+++ src/app/components/home/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  hambMenuOpen: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  toggleHambMenu() {
+    console.log('toggle acionado kkk: ', this.hambMenuOpen);
+    this.hambMenuOpen = !this.hambMenuOpen;
   }
 
 }
