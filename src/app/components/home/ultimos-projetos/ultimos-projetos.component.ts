dcc1751977df3a9c1a5a1591f220aa7d23eefb99
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2021-01-05 17:29:31 -0300

    Ultimos projetos atualizados

--- src/app/components/home/ultimos-projetos/ultimos-projetos.component.ts
+++ src/app/components/home/ultimos-projetos/ultimos-projetos.component.ts
@@ -1,15 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Projeto } from 'src/app/models/projeto.model';
+import { ProjetosService } from 'src/app/services/projetos.service';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-ultimos-projetos',
-  templateUrl: './ultimos-projetos.component.html',
-  styleUrls: ['./ultimos-projetos.component.css']
+    selector: 'app-ultimos-projetos',
+    templateUrl: './ultimos-projetos.component.html',
+    styleUrls: ['./ultimos-projetos.component.css']
 })
 export class UltimosProjetosComponent implements OnInit {
 
-  constructor() { }
+    projetos: Projeto[] = []
+    githubIcon = faGithub
+    faShare = faShare
 
-  ngOnInit(): void {
-  }
+    constructor(private service: ProjetosService) { }
 
+    ngOnInit(): void {
+        this.getProjetos()
+    }
+
+    getProjetos() {
+        this.service.getProjetos().subscribe(
+            result => {
+                let _result: any = result
+                _result.forEach(
+                    item => {
+                        let _projeto = new Projeto()
+                        _projeto.title = item['name']
+                        _projeto.description = item['description']
+                        _projeto.projeto_url = item['html_url']
+                        this.projetos.push(_projeto)
+                    }
+                )
+            }
+        )
+    }
 }
