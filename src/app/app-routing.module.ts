3b95bc41d106f4e468760f9359dd20ad486b73cc
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2021-01-05 18:30:20 -0300

    Routing terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
 import { HomeComponent } from './components/home/home.component';
 import { LinksComponent } from './components/links/links.component';
 
 const routes: Routes = [
-  { path: 'links', component: LinksComponent, },
-  { path: '', component: HomeComponent, },
-  { path: '**', redirectTo: '', },
+    { path: 'links', component: LinksComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
